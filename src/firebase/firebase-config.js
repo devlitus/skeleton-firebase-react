950c9b712eb6b20655d6e132ac08101102d65474
devlitus <developercarles@gmail.com>
2021-03-03 10:02:53 +0100

    configuration fireb ase

---
+++ src/firebase/firebase-config.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASEURLS,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGESENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const googleAuth = new firebase.auth.GoogleAuthProvider();
+const auth = firebase.auth();
+
+export{db, auth, googleAuth, firebase};
